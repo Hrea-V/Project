188ba1733cbbce85c4a14f0228bfb43d8cc3fc73
Hrea-V <151022572+Hrea-V@users.noreply.github.com>
2024-08-03 15:46:49 +0200

    0.00001 demo

--- MAINPROJECT/TFD Theorycraft Build/src/main/java/com/ironhack/tfdtheorycraftbuild/repositories/BuildsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ironhack.tfdtheorycraftbuild.repositories;
-
-
-import com.ironhack.tfdtheorycraftbuild.model.Build;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BuildsRepository extends JpaRepository<Build, Integer> {
-
-}
